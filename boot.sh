ffd5f183016012f2f2f4cfde5250f82bbca6904b
Aquib Mohudin <31084431+aqibbak@users.noreply.github.com>
2017-08-17 06:36:12 +0530

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -7,8 +7,6 @@ set -e
  printf 'EXTRA_DIST += \\\n' &&
  git ls-files debian | grep -v '^debian/\.gitignore$' |
  sed -e 's/\(.*\)/	\1 \\/' -e '$s/ \\//') > debian/automake.mk
-
 cat debian/control.in > debian/control
-
 # Bootstrap configure system from .ac/.am files
 autoreconf --install --force
